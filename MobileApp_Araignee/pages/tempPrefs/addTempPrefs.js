0d009b787ad319c7b7e0c8838e5359ebed444b80
NicFleurent <fleurent.nicolas@hotmail.com>
2025-03-03 13:07:43 -0500

    ajout des fichiers de pages

---
+++ MobileApp_Araignee/pages/tempPrefs/addTempPrefs.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const addTempPrefs = () => {
+  return (
+    <View>
+      <Text>addTempPrefs</Text>
+    </View>
+  )
+}
+
+export default addTempPrefs
